440c71e08425e37b3b3623107ca4c37bc60e9ef7
crazy2be <crazy1be@gmail.com>
2013-03-18 19:47:07 -0400

    Added basic transition chunking (very jumpy atm), moved cubicBezier out into it's own file.

--- src/iscroll.js
+++ src/iscroll.js
@@ -62,12 +62,13 @@
 '-transform';
 		elm.style[vendor + 'TransitionDuration'] = '0';
 		elm.style[vendor + 'TransformOrigin'] = '0 0';
-		elm.style[vendor + 'TransitionTimingFunction'] = 'cubic-bezier(0.33, 0.66, 0.66, 1)';
+		elm.style[vendor + 'TransitionTimingFunction'] = 'linear';
+// 		elm.style[vendor + 'TransitionTimingFunction'] = 'cubic-bezier(0.33, 0.66, 0.66, 1)';
 
 		function stop() {
-			cancelFrame(frameId);
+// 			cancelFrame(frameId);
 			steps = [];
-			cb();
+			running = false;
 		}
 
 		self.currentPosition = currentPosition;
@@ -96,8 +97,13 @@
 			if (!t) {
 				if (running) {
 					console.log("Attempt to move while scrolling");
+// 					forceStop(function () {
+// 						running = false;
+// 						moveTo(x, y, t);
+// 					})
 					// TODO: Smarter logic here
-					return;
+// 					return;
+					stop();
 				}
 				setTime(0);
 				setPosition(x, y, MODE_3D);
@@ -105,10 +111,32 @@
 			}
 			console.log("Actuall scrolling for a time: ", t, "ms");
 // 			running = false;
-			steps.push({
-				x: x, y: y, t: t,
-			});
+
+			var numSteps = Math.ceil(t / 100);
+			var curpos = currentPosition();
+			var dpos = {
+				x: (x - curpos.x) / numSteps,
+				y: (y - curpos.y) / numSteps,
+			};
+			for (var i = 0; i < numSteps; i++) {
+				curpos.x += dpos.x;
+				curpos.y += dpos.y;
+				steps.push({
+					x: curpos.x, y: curpos.y, t: t / numSteps,
+				});
+			}
 			start();
+		}
+
+		function makeSplits(numSteps, stepTime) {
+			if (numSteps === 1) {
+				return bezier;
+			}
+			var bits = bezier.split(0.5);
+			var res = [];
+			res = res.concat(makeSplit(numSteps - 1, stepTime, bits[0]));
+			res = res.concat(makeSplit(numSteps - 1, stepTime, bits[1]));
+			return res;
 		}
 
 		// Required hack to stop a scrolling animation currently in progress
@@ -116,7 +144,8 @@
 		function forceStop() {
 			// TODO: If animating, predict landing position and interpolate
 			// to there based on estimated lag.
-			var endpos = that.scroller.style[vendor + 'Transform'].replace(/[^0-9,-.]/g, '').split(',');
+			var curpos = currentPosition();
+			var endpos = elm.style[vendor + 'Transform'].replace(/[^0-9,-.]/g, '').split(',');
 			var end = {
 				x: parseFloat(endpos[0]),
 				y: parseFloat(endpos[1]),
@@ -179,77 +208,12 @@
 			if (mode === MODE_3D) {
 				var tr = 'translate3d(' + round(x) + 'px,' + round(y) + 'px,0px)';
 				elm.style[vendor + 'Transform'] = tr;
+			} else if (mode === MODE_2D) {
+				var tr = 'translate(' + round(x) + 'px,' + round(y) + 'px)';
+				elm.style[vendor + 'Transform'] = tr;
 			}
 			xpos = x;
 			ypos = y;
-		}
-	}
-
-	// Arguments coencide with the the arguments to the css interpolation
-	// function cubic-bezier().
-	function CubicBezier(p1x, p1y, p2x, p2y) {
-		var self = this;
-		var p0x = 0;
-		var p0y = 0;
-		var p3x = 1;
-		var p3y = 1;
-
-		// Compute the (x, y) value of the bezier curve at a given point.
-		// As with the css varient, t \in [0..1]
-		self.at = function (t) {
-			var p = Math.pow;
-
-			// This is just math, nothing tricky.
-			// http://en.wikipedia.org/wiki/B%C3%A9zier_curve#Cubic_B.C3.A9zier_curves
-			var c0 =     p(1 - t, 3);
-			var c1 = 3 * p(1 - t, 2) * t;
-			var c2 = 3 * (1 - t)   *  p(t, 2);
-			var c3 =                  p(t, 3);
-
-			return {
-				x: c0*p0x + c1*p1x + c2*p2x + c3*p3x,
-				y: c0*p0y + c1*p1y + c2*p2y + c3*p3y,
-			};
-		}
-
-		self.css = function () {
-			// p0 is fixed at (0, 0), and p3 is fixed at (1, 1) according to
-			// the CSS specifications.
-			return 'cubic-bezier(' + p1x + ',' + p1y + ',' + p2x + ',' + p2y + ')';
-		}
-
-		self.set = function (np0x, np0y, np1x, np1y, np2x, np2y, np3x, np3y) {
-			p0x = np0x; p0y = np0y;
-			p1x = np1x; p1y = np1y;
-			p2x = np2x; p2y = np2y;
-			p3x = np3x; p3y = np3y;
-		}
-
-		self.split = function (t) {
-			// http://en.wikipedia.org/wiki/De_Casteljau's_algorithm
-			var p01x = (p1x - p0x)*t + p0x
-			var p01y = (p1y - p0y)*t + p0x
-
-			var p12x = (p2x - p1x)*t + p1x
-			var p12y = (p2y - p1y)*t + p1y
-
-			var p23x = (p3x - p2x)*t + p2x
-			var p23y = (p3y - p2y)*t + p2y
-
-			var p012x = (p12x - p01x)*t + p01x
-			var p012y = (p12y - p01y)*t + p01y
-
-			var p123x = (p23x - p12x)*t + p12x
-			var p123y = (p23y - p12y)*t + p12y
-
-			var p0123x = (p123x - p012x)*t + p012x
-			var p0123y = (p123y - p012y)*t + p012y
-
-			var left = new CubicBezier();
-			left.set(p0x, p0y, p01x, p01y, p012x, p012y, p0123x, p0123y);
-			var right = new CubicBezier();
-			right.set(p0123x, p0123y, p123x, p123y, p23x, p23y, p3x, p3y);
-			return [left, right];
 		}
 	}
 
